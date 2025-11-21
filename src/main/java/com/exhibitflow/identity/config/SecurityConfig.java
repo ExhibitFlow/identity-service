60f4beb07f2143c426cbef8ba53ab88ea22eb489
Tharindu Jayawardhana <tharinduimalkajayawardhana@gmail.com>
2025-11-21 22:40:26 +0530

    feat: add CORS configuration to SecurityConfig for handling cross-origin requests

--- src/main/java/com/exhibitflow/identity/config/SecurityConfig.java
+++ src/main/java/com/exhibitflow/identity/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.exhibitflow.identity.config;
 import com.exhibitflow.identity.security.CustomUserDetailsService;
 import com.exhibitflow.identity.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +35,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+        .cors(
+            cors -> cors.configurationSource(request -> {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+                corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+                corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+                corsConfiguration.setAllowCredentials(true);
+                corsConfiguration.setExposedHeaders(List.of("Authorization"));
+                return corsConfiguration;
+            })
+        )
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
